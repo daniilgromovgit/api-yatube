db20114c56b80b539bc4fb98d60fea03347052de
pawnpunch <pawnpunch@gmail.com>
2026-07-02 22:41:40 +0300

    Successfull tests

---
+++ yatube_api/api/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import get_object_or_404
+from posts.models import Comment, Group, Post
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from api.permissions import IsAuthorOrReadOnly
+from api.serializers import (
+    CommentSerializer,
+    GroupSerializer,
+    PostSerializer,
+)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
+
+    def get_queryset(self):
+        return Comment.objects.filter(post_id=self.kwargs['post_id'])
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        serializer.save(author=self.request.user, post=post)
